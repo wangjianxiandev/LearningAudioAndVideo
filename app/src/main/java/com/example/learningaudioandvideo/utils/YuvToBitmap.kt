369f240077b5f85fc1875aad80cc3344a4761dd2
wangjianxiandev <jianxianwang198@163.com>
2021-01-23 21:52:05 +0800

    Yuv convert to bitmap and save it to file

--- app/src/main/java/com/example/learningaudioandvideo/utils/YuvToBitmap.kt
+++ app/src/main/java/com/example/learningaudioandvideo/utils/YuvToBitmap.kt
@@ -1,27 +1,44 @@
 package com.example.learningaudioandvideo.utils
 
+import android.content.Context
 import android.graphics.*
+import android.os.Environment
+import android.util.Log
 import android.util.Size
-import java.io.ByteArrayOutputStream
+import java.io.*
 
 /**
  * Created with Android Studio.
- * Description:
+ * Description: Yuv convert to bitmap and save it to file
  * @author: Wangjianxian
  * @CreateDate: 2021/1/21 0:24
  */
 object YuvToBitmap {
-    fun YuvToBitmap(data: ByteArray, size: Size) {
-        val yuvImage = YuvImage(data, ImageFormat.NV21, size.width, size.height, null)
-        if (yuvImage != null) {
-            var byteArrayOutputStream: ByteArrayOutputStream = ByteArrayOutputStream()
-            yuvImage.compressToJpeg(Rect(0, 0, size.width, size.height), 80, byteArrayOutputStream)
-            val bitmap: Bitmap = BitmapFactory.decodeByteArray(
-                byteArrayOutputStream.toByteArray(),
-                0,
-                byteArrayOutputStream.size()
-            )
-            byteArrayOutputStream.close()
+    fun YuvToBitmap(data: ByteArray, size: Size, context : Context) {
+        val dirPath = File(Environment.getExternalStorageDirectory(), "wjx")
+        if (!dirPath.exists()) {
+            dirPath.mkdir()
+        }
+        val fileName = System.currentTimeMillis().toString() + ".jpeg"
+        val file = File(dirPath, fileName)
+        try {
+            val fos = FileOutputStream(file)
+            val yuvImage = YuvImage(data, ImageFormat.NV21, size.width, size.height, null)
+            if (yuvImage != null) {
+                var byteArrayOutputStream: ByteArrayOutputStream = ByteArrayOutputStream()
+                yuvImage.compressToJpeg(Rect(0, 0, size.width, size.height), 80, byteArrayOutputStream)
+                BitmapFactory.decodeByteArray(
+                    byteArrayOutputStream.toByteArray(),
+                    0,
+                    byteArrayOutputStream.size()
+                ).compress(Bitmap.CompressFormat.JPEG, 50, fos)
+                fos.flush()
+                fos.close()
+            }
+        } catch (e : FileNotFoundException) {
+            Log.e("WJX", "file is not found")
+        } catch (e : IOException) {
+            Log.e("WJX", "Io exception")
         }
     }
 }
